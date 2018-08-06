1c7f236715c2d1232dffa934534374e452846ba8
Jprimero15 <jprimero155@gmail.com>
2018-08-06 21:58:29 +0000

    aosip-fy

--- aosip.mk
+++ aosip.mk
@@ -1,5 +1,6 @@
-# Copyright (C) 2013-2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +13,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#
 
 $(call inherit-product, device/samsung/hltechn/full_hlte.mk)
 
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hltechn
-PRODUCT_NAME := lineage_hltechn
+PRODUCT_NAME := aosip_hltechn
+
